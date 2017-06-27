b84be3a657d3e6564776bc30251d5926ea617fec
KevinBehlmaier <kevin.behlmaier@gmail.com>
2017-06-26 20:26:46 -0600

    Adds ex3.rb

---
+++ ex3.rb
@@ -0,0 +1,48 @@
+#this line talks about counting chickens
+puts "I will now count my chickens:"
+
+#this line adds up my Hens
+puts "Hens #{25.0 + 30.0 / 6.0}"
+#this line adds up my roosters
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+
+#this line is stating that I will now count eggs
+puts "Now I will count the eggs:"
+
+#This is my addings of eggs
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+#this is me questioning math
+puts "Is that true that 3.0 + 2.0 < 5.0 - 7.0?"
+
+#this line runs code without quotes
+puts 3.0 + 2.0 < 5.0 - 7.0
+
+#this line is my breaking down math, and running code in a string
+puts "What is 3.0 + 2.0? #{3.0 + 2.0}"
+#this line is my breaking down math, and running code in a string with #
+puts "What is 5.0 - 7.0? #{5.0 - 7.0}"
+
+#this is the realization
+puts "Oh, that's why it's false."
+
+#asking for more here
+puts "How about some more."
+
+#more lines with strings runnning math code
+puts "Is it greater? #{5.0 > -2.0}"
+#line with running math code using #
+puts "Is it greater or equal? #{5.0 >= -2.0}"
+#line with running math code using #
+puts "Is it less or equal? #{5.0 <= -2.0}"
+
+# *this is my Study Drills new .rb file, because I already have one file for each example*
+# I need something to calculate!!!!
+# Ah! The Money I have made on harvesting!
+puts "Oh, what a glorious day!!! Let me count paper!"
+
+puts 180 + 40 + 40 + 40 + 60 + 300 + 120 + 140 + 40 + 60 + 50 + 40 + 100 + 40 + 80 + 40 + 120 + 40 + 140
+
+puts "My cost is $750 max. How much am I getting in return?"
+
+puts (180 + 40 + 40 + 40 + 60 + 300 + 120 + 140 + 40 + 60 + 50 + 40 + 100 + 40 + 80 + 40 + 120 + 40 + 140) - 750
