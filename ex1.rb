ff19c2bf9611f95875b99b528c611cebff905c10
KevinBehlmaier <kevin.behlmaier@gmail.com>
2017-06-26 20:14:09 -0600

    Adds ex1.rb

---
+++ ex1.rb
@@ -0,0 +1,7 @@
+#puts "Hello World!"
+#puts "I like typing this."
+#puts "This is fun."
+#puts "Yay! Printing."
+#puts "I'd Much rather you 'not'."
+#puts 'I "said" do not touch this.'
+puts "you think your smart gravy, huh?"
